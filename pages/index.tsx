3d448ddda5acbe6589beeacfbeb925498dab4138
Anurag Mishra <97835032+shaktimaan00@users.noreply.github.com>
2024-02-13 22:45:34 +0530

    Update meta information in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -107,8 +107,8 @@ export default function Home() {
 
   console.log("website is rendering...");
   const meta = {
-    title: "Abdellatif Anaflous - Software Engineer",
-    description: `I've been working on Software development for 5 years straight. Get in touch with me to know more.`,
+    title: "Anurag Mishra - Software Engineer",
+    description: ``,
     image: "/titofCercle.png",
     type: "website",
   };
@@ -120,7 +120,7 @@ export default function Home() {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://anaflous.com`} />
+        {/* <meta property="og:url" content={`https://anaflous.com`} /> */}
         <link rel="canonical" href={`https://anaflous.com`} />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Anurag Mishra" />
